198721c715051c71951a1bec33f91de591bba334
José Carlos Pesina Siller <pesinasiller@MacBook-pesinasiller.local>
2023-04-18 20:35:25 -0400

    feat: bootstrap sanity studio

---
+++ sanity.config.js
@@ -0,0 +1,23 @@
+import {defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+import {visionTool} from '@sanity/vision'
+//import {googleMapsInput} from '@sanity/google-maps-input'
+import {schemaTypes} from './schemas'
+
+export default defineConfig({
+  name: 'default',
+  title: 'radionopal-sanity',
+
+  projectId: '3bcawm4g',
+  dataset: 'production',
+
+  plugins: [
+    deskTool(),
+    visionTool(),
+    //googleMapsInput(),
+  ],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
